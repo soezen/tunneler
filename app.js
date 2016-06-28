460075736dcc1b6b631a22f4c0224721722f9fa3
soezen <rogge.suzan@gmail.com>
2016-06-28 10:48:38 +0200

    change port to env property if it exists

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var game = new tunneler.Game();
 
 app.use('/tunneler', express.static(__dirname + '/client'));
 
-http.listen(8080, function() {
-  console.log('listening on http://localhost:8080/tunneler');
+http.listen(process.env.PORT || 3000, function() {
+  console.log('listening on http://localhost:' + (process.env.PORT || 3000) + '/tunneler');
 });
 
 io.on('connection', function(socket) {
